c7681da68eda97f4cae72c86206b3acd8c77cb7b
Markus Schepke <markus.schepke@wolt.com>
2024-11-09 23:03:46 +0200

    Added --overwrite and check for existing files

--- src/board_game_merger/__main__.py
+++ src/board_game_merger/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import sys
+from pathlib import Path
 
 from board_game_merger.config import MergeConfig
 from board_game_merger.merge import merge_files
@@ -48,6 +49,12 @@ def _parse_args() -> argparse.Namespace:
         help="Minimum number of days for the latest column",
     )
     parser.add_argument(
+        "--overwrite",
+        "-W",
+        action="store_true",
+        help="Overwrite output file if it exists",
+    )
+    parser.add_argument(
         "--progress-bar",
         "-p",
         action="store_true",
@@ -75,15 +82,22 @@ def main() -> None:
     for site in args.sites:
         LOGGER.info("Merging board game data from site <%s>", site)
 
+        merge_config = MergeConfig.site_config(
+            site=site,
+            item=args.item_type,
+            in_paths=args.in_paths,
+            out_path=args.out_path,
+            clean_results=args.clean_results,
+            latest_min_days=args.latest_min_days,
+        )
+
+        out_path = Path(merge_config.out_path).resolve()
+        if out_path.exists() and not args.overwrite:
+            LOGGER.warning("Output file <%s> exists, skipping", out_path)
+            continue
+
         merge_files(
-            merge_config=MergeConfig.site_config(
-                site=site,
-                item=args.item_type,
-                in_paths=args.in_paths,
-                out_path=args.out_path,
-                clean_results=args.clean_results,
-                latest_min_days=args.latest_min_days,
-            ),
+            merge_config=merge_config,
             drop_empty=True,
             sort_keys=bool(args.clean_results),
             progress_bar=bool(args.progress_bar),
